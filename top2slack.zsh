0144a9b8fdc499a6ce0681a546314b9cac88bd25
Drake Sobania <dsobania3@gatech.edu>
2020-04-16 22:04:42 -0400

    send the update to the file

--- top2slack.zsh
+++ top2slack.zsh
@@ -2,7 +2,7 @@
 # assumes postToSlack.sh from https://github.com/sulhome/bash-slack is at ~
 
 # update
-./parse.zsh
+./parse.zsh >> covid.log
 
 # post
 ~/postToSlack.sh -t "$(echo $(date +%d.%m.%y-%H:%M:%S))" -b "Top Case Increases:"$'\n'"$(perl ./top_10.pl covid.log)" -c "caseupdates" -u "$1"
