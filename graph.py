4c15d44b939b55cb9a2d088f27306c0eff2c243f
Drake Sobania <dsobania3@gatech.edu>
2020-04-26 18:03:48 -0400

    only color top counties

--- graph.py
+++ graph.py
@@ -1,13 +1,26 @@
 import matplotlib.pyplot as plt
-import matplotlib.pylab as pylab
 import sqlite3
 
 connection = sqlite3.connect('caseupdates.sqlite')
 cursor = connection.cursor()
-cursor.execute("""SELECT COUNTY, CASES
-FROM CASES ORDER BY SET_ID ASC""");
+cursor.execute("""SELECT
+       COUNTY,
+       CASES,
+       CASE
+           WHEN COUNTY IN (
+               SELECT DISTINCT(COUNTY)
+               FROM CASES ORDER BY CASES DESC
+               LIMIT 10
+               ) THEN 1
+           ELSE 0
+           END
+        AS IMPORTANT
+FROM CASES
+WHERE UPPER(COUNTY)!='UNKNOWN'
+ORDER BY SET_ID ASC""")
 
 counties = []
+important_counties = []
 index = None
 counts = []
 while True:
@@ -15,11 +28,13 @@ while True:
     if row is None:
         break
     else:
-        (county, cases) = row
+        (county, cases, important) = row
     if county not in counties:
         counties.append(county)
         counts.append([])
         index = len(counties) - 1
+        if important == 1:
+            important_counties.append(county)
     else:
         for i in range(len(counties)):
             if county == counties[i]:
@@ -28,18 +43,17 @@ while True:
     counts[index].append(cases)
 
 # based on https://stackoverflow.com/questions/4534480/get-legend-as-a-separate-picture-in-matplotlib
-figData = pylab.figure(figsize=(8.5, 11))
-ax = pylab.gca()
-
+fig, ax = plt.subplots(figsize=(11, 11))
+ax.set_title('Cumulative Cases By GA County')
 for i in range(len(counts)):
     x = []
     y = []
     for j in range(len(counts[i])):
         x.append(j)
         y.append(counts[i][j])
-    pylab.plot(x, y, label=counties[i])
-
-figLegend = pylab.figure(figsize=(8.5, 11))
-pylab.figlegend(*ax.get_legend_handles_labels(), loc='upper left', ncol=5)
-figData.show()
-figLegend.show()
+    if counties[i] in important_counties:
+        ax.plot(x, y, label=counties[i])
+    else:
+        ax.plot(x, y, color='#000000', label='_nolegend_')
+ax.legend()
+fig.show()
