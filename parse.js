eeb26221c6b739bd1899068e6fe38662525531bd
Drake Sobania <dsobania3@gatech.edu>
2020-05-10 23:24:00 -0400

    new parse done for now

--- parse.js
+++ parse.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   try {
     const browser = await puppeteer.launch({
-      headless: false
+      // headless: false
     });
     const page = await browser.newPage();
     await page.goto('https://ga-covid19.ondemand.sas.com/');
@@ -14,13 +14,21 @@ const puppeteer = require('puppeteer');
           visible: true,
         });
     const html = await page.content();
-    const myregexp = /<td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignLeft MuiTableCell-sizeSmall" style="border: none;">(\w+)<\/td><td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall" style="border: none;">(\d+)<\/td><td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall" style="border: none;">[.\d-]+<\/td><td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall" style="border: none;">(\d+)/g;
+    const myregexp = /<td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignLeft MuiTableCell-sizeSmall" style="border: none;">([a-zA-Z ]+)<\/td><td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall" style="border: none;">(\d+)<\/td><td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall" style="border: none;">[.\d-]+<\/td><td class="MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall" style="border: none;">(\d+)/g;
     let match = myregexp.exec(html);
+    const db = require('better-sqlite3')('caseupdates.sqlite');
+    const row = await db.prepare('select max(set_id) as setId from CASES').get();
+    const setId = row.setId + 1;
+    const insert = db.prepare('INSERT INTO CASES (DATETIME, COUNTY, CASES, DEATHS, SET_ID) VALUES (?, ?, ?, ?, ?)');
     while (match != null) {
-      console.log(`${match[1]}:${match[2]}:${match[3]}`);
+      const d = new Date();
+      const datestring = (d.getFullYear()) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-"
+          + ("0" + d.getDate()).slice(-2) + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2)
+          + ":" + ("0" + d.getSeconds()).slice(-2) + ".000000";
+      insert.run(datestring, `${match[1]}`, `${match[2]}`, `${match[3]}`, setId);
       match = myregexp.exec(html);
     }
-
+    await db.close();
     await browser.close();
   } catch (error) {
     console.log(error);
